b60e6eb9af6f0a97cb464c7e5688e46e8e44f8a2
Dan Leach <web@danleach.uk>
2025-10-25 06:53:29 +0100

    Initial PWA

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,75 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: "autoUpdate", // updates service worker automatically
+      includeAssets: [
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+        "pwa-192x192.png",
+        "pwa-512x512.png",
+      ],
+      manifest: {
+        name: "MSF Diabetes Calculator",
+        short_name: "MSF Diabetes",
+        id: "msf-diabetes-calculator-v0.2",
+        description:
+          "The MSF Diabetes Calculator allows clinicians to calculate variables for managing paediatric diabetic ketoacidosis based on the 2024 MSF paediatric guidelines.",
+        start_url: "/",
+        launch_handler: {
+          client_mode: ["navigate-existing", "auto"],
+        },
+        handle_links: "preferred",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ec0000",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.yourdomain\.com\/.*/i,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              networkTimeoutSeconds: 10,
+              expiration: { maxEntries: 50, maxAgeSeconds: 86400 },
+              cacheableResponse: { statuses: [0, 200] },
+            },
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
